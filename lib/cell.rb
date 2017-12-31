1a079137e2c9163e97e2efebeb7003526cf5d296
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-12-31 18:28:07 +0000

    Add filled_out? for cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,12 @@
 class Cell
   attr_reader :value
-  
+
   def initialize(value)
     @value = value
+  end
+
+  def filled_out?
+    @value != 0
   end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,4 +8,10 @@ describe Cell do
     cell = Cell.new(1)
     expect(cell.value).to eq(1)
   end
-end 
+
+  it 'should know it has been filled' do
+    cell = Cell.new(5)
+    expect(cell).to be_filled_out
+  end
+  
+end
